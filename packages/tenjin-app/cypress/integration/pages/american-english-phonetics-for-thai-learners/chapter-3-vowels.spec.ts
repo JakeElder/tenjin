277ea964787d77ba67b03b7c600cc756b468a46f
Jake Elder <jake@jakes.world>
2020-06-30 18:55:32 +0700

    feat: pull chapter data from api

--- packages/tenjin-app/cypress/integration/pages/american-english-phonetics-for-thai-learners/chapter-3-vowels.spec.ts
+++ packages/tenjin-app/cypress/integration/pages/american-english-phonetics-for-thai-learners/chapter-3-vowels.spec.ts
@@ -1,9 +1,13 @@
 const URL = "/american-english-phonetics-for-thai-learners/chapter-3-vowels";
 
 describe("Chapter 3", () => {
-  it("Has the correct title", () => {
+  it("Has the correct titles", () => {
+    cy.server();
+    cy.route("POST", "/graphql", "fixture:chapters.json").as("chapter");
     cy.visit(URL);
+    cy.wait("@chapter");
     cy.get("h1").contains("American English Phonetics for Thai Learners");
+    cy.get("h2").contains("Chapter 3: Vowels");
   });
 });
 
