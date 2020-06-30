cd735ddfd9ed43f73954d4ca01f4043f03593bb0
Jake Elder <jake@jakes.world>
2020-06-30 14:54:42 +0700

    feat(app): add aepftl/c3 route

---
+++ packages/tenjin-app/cypress/integration/pages/american-english-phonetics-for-thai-learners/chapter-3-vowels.spec.ts
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+const URL = "/american-english-phonetics-for-thai-learners/chapter-3-vowels";
+
+describe("Chapter 3", () => {
+  it("Has the correct title", () => {
+    cy.visit(URL);
+    cy.get("h1").contains("American English Phonetics for Thai Learners");
+  });
+});
+
+export {};
---
+++ packages/tenjin-app/src/pages/american-english-phonetics-for-thai-learners/chapter-3-vowels.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Chapter3: React.FC<Props> = () => {
+  return <h1>American English Phonetics for Thai Learners</h1>;
+};
+
+export default Chapter3;
