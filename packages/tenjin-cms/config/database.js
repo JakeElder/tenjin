b78e03c54e7fc66e40df6b4534e286745b7a7fd4
Jake Elder <jake@jakes.world>
2020-07-01 10:05:11 +0700

    chore(cms): debug Heroku build env

--- packages/tenjin-cms/config/database.js
+++ packages/tenjin-cms/config/database.js
@@ -1,16 +1,31 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "mongoose",
-      settings: {
-        client: "mongo",
-        uri: env("DATABASE_URI"),
-      },
-      options: {
-        authenticationDatabase: "admin",
-        ssl: env.NODE_ENV === "stage" || env.NODE_ENV === "production",
+module.exports = ({ env }) => {
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log(env.NODE_ENV);
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  console.log("------");
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "mongoose",
+        settings: {
+          client: "mongo",
+          uri: env("DATABASE_URI"),
+        },
+        options: {
+          authenticationDatabase: "admin",
+          ssl: env.NODE_ENV === "stage" || env.NODE_ENV === "production",
+        },
       },
     },
-  },
-});
+  };
+};
