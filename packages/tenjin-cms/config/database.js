cefe32331e5836aee758d4cf4feebf8f677d0364
Jake Elder <jake@jakes.world>
2020-07-01 10:14:17 +0700

    chore(cms): use process.env for Heroku build

--- packages/tenjin-cms/config/database.js
+++ packages/tenjin-cms/config/database.js
@@ -1,31 +1,16 @@
-module.exports = ({ env }) => {
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log(env.NODE_ENV);
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  console.log("------");
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "mongoose",
-        settings: {
-          client: "mongo",
-          uri: env("DATABASE_URI"),
-        },
-        options: {
-          authenticationDatabase: "admin",
-          ssl: env.NODE_ENV === "stage" || env.NODE_ENV === "production",
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        client: "mongo",
+        uri: env("DATABASE_URI"),
+      },
+      options: {
+        authenticationDatabase: "admin",
+        ssl: ["stage", "production"].includes(process.env.NODE_ENV),
       },
     },
-  };
-};
+  },
+});
