28dd2666c4addf230d55e60eb486422f4a3154bb
Jake Elder <jake@jakes.world>
2020-07-03 18:29:56 +0700

    feat(ui): add vertical rhythm tools

--- packages/tenjin-ui/stories/Theme.stories.tsx
+++ packages/tenjin-ui/stories/Theme.stories.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import Theme from "../src/Theme";
+import styled from "styled-components";
 
 export default {
   title: "Theme",
   component: Theme,
 };
 
+const Wrapper = styled.div`
+  margin-left: 30px;
+  margin-right: 30px;
+`;
+
+const H1 = styled.h1`
+  margin-top: ${(props) => props.theme.rhythmSizing(1)}rem;
+  ${(props) => props.theme.setFontWithRhythm("Fantasque", 3)}
+`;
+
+const P1 = styled.p`
+  margin-top: ${(props) => props.theme.rhythmSizing(2)}rem;
+  ${(props) => props.theme.setFontWithRhythm("Fantasque", 1, 1.2)}
+`;
+
+const P2 = styled.p`
+  margin-top: ${(props) => props.theme.rhythmSizing(2)}rem;
+  ${(props) => props.theme.setFontWithRhythm("Fantasque", 1)}
+`;
+
 export const Default = () => (
   <Theme>
-    <h1>Theme</h1>
-    <p>This should be Fantasque</p>
-    <p>There should be no default margins</p>
+    <Wrapper>
+      <H1>Theme</H1>
+      <P1>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt.
+      </P1>
+      <P2>There should be no default margins</P2>
+    </Wrapper>
   </Theme>
 );
