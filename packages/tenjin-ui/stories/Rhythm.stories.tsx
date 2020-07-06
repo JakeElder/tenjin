2c42522c5d24f318f7c5423f39c7a1b84c2cffcb
Jake Elder <jake@jakes.world>
2020-07-06 10:31:44 +0700

    chore(ui): remove Rhytm story

--- packages/tenjin-ui/stories/Rhythm.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Rhythm from "../src/components/Rhythm";
-
-export default {
-  title: "Rhythm",
-  component: Rhythm,
-};
-
-export const Default = () => <Rhythm />;
