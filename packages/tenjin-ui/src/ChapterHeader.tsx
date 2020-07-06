ca64e1b7400241ffc9c66dfc2b6ff48b8c7031d9
Jake Elder <jake@jakes.world>
2020-07-06 10:16:41 +0700

    feat(ui): style ChapterHeader

--- packages/tenjin-ui/src/ChapterHeader.tsx
+++ packages/tenjin-ui/src/ChapterHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 type Props = {
   work: string;
@@ -6,13 +7,27 @@ type Props = {
   chapter: string;
 };
 
+const Work = styled.h1`
+  ${(props) => props.theme.setFontWithRhythm("Fantasque", 2.125, 1)}
+`;
+
+const Author = styled.div`
+  margin-top: ${(props) => props.theme.rhythmSizing(1)}rem;
+  color: #959595;
+`;
+
+const ChapterTitle = styled.h2`
+  margin-top: ${(props) => props.theme.rhythmSizing(4)}rem;
+  ${(props) => props.theme.setFontWithRhythm("Fantasque", 2, 1)}
+`;
+
 const ChapterHeader: React.FC<Props> = ({ work, author, chapter }) => {
   return (
-    <div>
-      {work}
-      {author}
-      {chapter}
-    </div>
+    <>
+      <Work>{work}</Work>
+      <Author>by {author}</Author>
+      <ChapterTitle>{chapter}</ChapterTitle>
+    </>
   );
 };
 
