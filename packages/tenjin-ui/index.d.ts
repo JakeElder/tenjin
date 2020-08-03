7c47671eedc0b49c95d8b16f4a20f411091200c3
Jake Elder <jake@jakes.world>
2020-08-03 16:44:18 +0700

    refactor(ui): remove rhytm stuff

--- packages/tenjin-ui/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare module "@ceteio/styled-components-rhythm";
-declare module "cap-height/dist/cap-height";
--- packages/tenjin-ui/stories/Theme.stories.tsx
+++ packages/tenjin-ui/stories/Theme.stories.tsx
@@ -7,47 +7,19 @@ export default {
   component: Theme,
 };
 
-const Wrapper = styled.div`
-  margin-left: 30px;
-  margin-right: 30px;
-`;
-
-const H1 = styled.h1`
-  margin-top: ${(props) => props.theme.rhythmSizing(1)}rem;
-  ${(props) => props.theme.setFontWithRhythm("Fantasque", 3, 1)}
-`;
-
-const rhythmic = {
-  height: (units: number) => {
-    return (props: any) => {
-      return `height: ${props.theme.rhythmSizing.call(props.theme, units)}rem`;
-    };
-  },
-};
-
-const P1 = styled.p`
-  margin-top: ${(props) => props.theme.rhythmSizing(2)}rem;
-  ${(props) => props.theme.setFontWithRhythm("Fantasque", 1)}
-`;
-
-const P2 = styled.p`
-  margin-top: ${(props) => props.theme.rhythmSizing(2)}rem;
-  ${(props) => props.theme.setFontWithRhythm("Fantasque", 1)}
-`;
+const Wrapper = styled.div``;
 
 export const Default = () => (
-  // <Theme> <-- Globally wraps all components
   <Wrapper>
-    <H1>Theme</H1>
-    <P1>
+    <h1>Theme</h1>
+    <p>
       Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
       doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
       inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
       Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
       fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
       sequi nesciunt.
-    </P1>
-    <P2>There should be no default margins</P2>
+    </p>
+    <p>There should be no default margins</p>
   </Wrapper>
-  // </Theme>
 );
