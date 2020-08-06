e794a428ab3d7a82acd800fde7acd6a265bdea7f
Jake Elder <jake@jakes.world>
2020-08-06 19:46:09 +0700

    refactor(ui): add Typography story

---
+++ packages/tenjin-ui/src/components/Typography.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { typography, color } from "styled-system";
+import shouldForwardProp from "@styled-system/should-forward-prop";
+
+type HeadingProps = {
+  children: string;
+  h: 1 | 2 | 3 | 4 | 5 | 6;
+};
+
+type HeadingFactoryProps = {
+  fontSize?: number;
+  color?: string;
+};
+
+const Heading = styled("h1", { shouldForwardProp })(typography, color);
+
+function headingFactory({ fontSize, color }: HeadingFactoryProps) {
+  const R = ({ children, h }: HeadingProps) => (
+    <Heading as={`h${h}`} fontSize={fontSize} color={color}>
+      {children}
+    </Heading>
+  );
+  R.defaultProps = { h: 1 } as Partial<HeadingProps>;
+  return R;
+}
+
+export const LongformHeading = headingFactory({
+  fontSize: 4,
+  color: "shades.0",
+});
---
+++ packages/tenjin-ui/stories/Typography.stories.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { LongformHeading } from "../src/components/Typography";
+
+export default {
+  title: "Foundation | Typography",
+};
+
+type TextStyle = {
+  ids: string[];
+  Component: React.ReactType;
+  description: string;
+};
+
+const textStyles: TextStyle[] = [
+  {
+    ids: ["Longform Heading", "Exercise Name"],
+    Component: LongformHeading,
+    description: "Fantasque Sans Mono Regular 32px #000",
+  },
+];
+
+const Table = styled.table`
+  width: 100%;
+`;
+
+const TR = styled.tr`
+  border-bottom: 1px solid #d9d9d9;
+`;
+
+const IDTD = styled.td`
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  background-color: #f8f8f8;
+  color: #777;
+  padding: 26px 20px;
+`;
+
+const DescriptionTD = styled.td`
+  vertical-align: middle;
+  padding-left: 22px;
+`;
+
+export const Default = () => (
+  <Table>
+    <tbody>
+      {textStyles.map(({ ids, Component, description }) => {
+        return (
+          <TR key={ids.join()}>
+            <IDTD>
+              {ids.map((id) => (
+                <div style={{ marginBottom: 4 }} key={id}>
+                  {id}
+                </div>
+              ))}
+            </IDTD>
+            <DescriptionTD>
+              <Component>{description}</Component>
+            </DescriptionTD>
+          </TR>
+        );
+      })}
+    </tbody>
+  </Table>
+);
