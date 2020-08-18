aa9094e3bd92ff19a10e1170849e7033ff4c4277
Jake Elder <jake@jakes.world>
2020-08-18 09:57:21 +0700

    feat(ui): Typography [WIP]

--- packages/tenjin-ui/.storybook/main.js
+++ packages/tenjin-ui/.storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   stories: ["../stories/**/*.stories.tsx"],
   addons: ["@storybook/preset-typescript"],
+  webpackFinal: async (config) => {
+    config.module.rules[0].use[0].options.presets.push(
+      require.resolve("@emotion/babel-preset-css-prop")
+    );
+    return config;
+  },
 };
--- packages/tenjin-ui/stories/Typography.stories.tsx
+++ packages/tenjin-ui/stories/Typography.stories.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
 import {
-  Button,
   LongformHeading,
-  // LongformDetail,
-  // LongformSubheading,
-  // LongformCopy,
-  // FigCaption,
-  // LongformSectionHeading,
-  // ChecklistHeading,
+  LongformDetail,
+  LongformSubheading,
+  LongformCopy,
+  FigCaption,
+  LongformSectionHeading,
+  ChecklistHeading,
 } from "../src/components/Typography";
 
 export default {
@@ -27,37 +26,37 @@ const textStyles: TextStyle[] = [
     Component: LongformHeading,
     description: "Fantasque Sans Mono Regular 32px #000",
   },
-  // {
-  //   ids: ["Longform Detail", "Exercise Subheading"],
-  //   Component: LongformDetail,
-  //   description: "Fantasque Sans Mono Regular 20px #999",
-  // },
-  // {
-  //   ids: ["Longform Subheading", "Exercise Heading"],
-  //   Component: LongformSubheading,
-  //   description: "Fantasque Sans Mono Bold 24px #000",
-  // },
-  // {
-  //   ids: ["Longform Copy"],
-  //   Component: LongformCopy,
-  //   description:
-  //     "Fantasque Sans Mono regular 16px #222. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. ",
-  // },
-  // {
-  //   ids: ["Fig Caption"],
-  //   Component: FigCaption,
-  //   description: "Fantasque Sans Mono regular 16px #888",
-  // },
-  // {
-  //   ids: ["Longform Section Heading"],
-  //   Component: LongformSectionHeading,
-  //   description: "Fantasque Sans Mono Bold 24px #32B66B",
-  // },
-  // {
-  //   ids: ["Checklist Heading"],
-  //   Component: ChecklistHeading,
-  //   description: "Fantasque Sans Mono Regular 20px #555",
-  // },
+  {
+    ids: ["Longform Detail", "Exercise Subheading"],
+    Component: LongformDetail,
+    description: "Fantasque Sans Mono Regular 20px #999",
+  },
+  {
+    ids: ["Longform Subheading", "Exercise Heading"],
+    Component: LongformSubheading,
+    description: "Fantasque Sans Mono Bold 24px #000",
+  },
+  {
+    ids: ["Longform Copy"],
+    Component: LongformCopy,
+    description:
+      "Fantasque Sans Mono regular 16px #222. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. ",
+  },
+  {
+    ids: ["Fig Caption"],
+    Component: FigCaption,
+    description: "Fantasque Sans Mono regular 16px #888",
+  },
+  {
+    ids: ["Longform Section Heading"],
+    Component: LongformSectionHeading,
+    description: "Fantasque Sans Mono Bold 24px #32B66B",
+  },
+  {
+    ids: ["Checklist Heading"],
+    Component: ChecklistHeading,
+    description: "Fantasque Sans Mono Regular 20px #555",
+  },
 ];
 
 const Table = styled.table`
@@ -82,4 +81,25 @@ const DescriptionTD = styled.td`
   padding-left: 22px;
 `;
 
-export const Default = () => <Button>text</Button>;
+export const Default = () => (
+  <Table>
+    <tbody>
+      {textStyles.map(({ ids, Component, description }) => {
+        return (
+          <TR key={ids.join()}>
+            <IDTD>
+              {ids.map((id) => (
+                <div style={{ marginBottom: 4 }} key={id}>
+                  {id}
+                </div>
+              ))}
+            </IDTD>
+            <DescriptionTD>
+              <Component as="h3">{description}</Component>
+            </DescriptionTD>
+          </TR>
+        );
+      })}
+    </tbody>
+  </Table>
+);
